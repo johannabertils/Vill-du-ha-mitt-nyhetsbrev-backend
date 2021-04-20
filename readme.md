ab569f6869cfaf7518d5de95c0ac20c8665c75dd
Johanna Bertils <johannabertils@hotmail.com>
2021-04-20 09:45:32 +0200

    creating users.json and connecting it to frontend

---
+++ readme.md
@@ -0,0 +1,10 @@
+install:
+
+express:
+npx express-generator --no-view  
+Npm install 
+Npm start
+Gå in på localhost:3000 i webbläsaren
+
+cors:
+ npm install cors
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,23 @@
 var express = require('express');
 var router = express.Router();
+const fs = require("fs");
+const cors = require("cors");
+
+router.use(cors()); 
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/', function (req, res, next) {
+
+  fs.readFile("users.json", function (err, data) {
+    if (err) {
+      console.log(err);
+    }
+
+    let users = JSON.parse(data);
+
+    res.json(users);
+  });
+
 });
 
 module.exports = router;
